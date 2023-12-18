d19e17e0e4d41ec072097e378d3b077ba00b0ca9
Kashfi Fahim <Kashfi.Fahim@gmail.com>
2023-12-18 18:20:13 -0500

    set up Alpha Vantage API integration then fetch stock data and update db

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
+import requests
 import sqlite3 
 
 app = Flask(__name__)
+
+
+def schedule_stock_updates():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=update_all_stocks, trigger="interval", minutes=15)
+    scheduler.start()
+
+
+def update_all_stocks():
+    api_key = 'Z26IND1A7LZTHK7V'
+    symbols = ['TSLA', 'GOOGL', 'AMZN', 'AAPL', 'CSCO', 'NOK']
+    for symbol in symbols:
+        price = fetch_stock_price(symbol, api_key)
+        update_stock_price(symbol, price)
+
+
+def fetch_stock_price(symbol, api_key):
+    url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={api_key}'
+    response = requests.get(url)
+    data = response.json()
+    price = data["Global Quote"]["05. price"]
+    return float(price)
+
+
+def update_stock_price(symbol, price):
+    conn = get_db_connection()
+    conn.execute('UPDATE stocks SET PRICE = ?, last_update = CURRENT_TIMESTAMP WHERE symbol = ?', (price, symbol))
+    conn.commit()
+    conn.close()
 
 
 def get_db_connection():
@@ -30,4 +61,5 @@ def stocks():
 
 
 if __name__ == '__main__':
+    schedule_stock_updates()
     app.run(debug=True)
